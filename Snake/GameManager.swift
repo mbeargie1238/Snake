287292bbc5997d156289f2bddad785b4588fe662
user146479 <user146479@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-12-05 15:51:17 -0500

    initial pull

---
+++ Snake/GameManager.swift
@@ -0,0 +1,107 @@
+//
+//  GameManager.swift
+//  Snake
+//
+//  Created by user146479 on 12/5/18.
+//  Copyright © 2018 Matt Beargie. All rights reserved.
+//
+
+import SpriteKit
+
+//2 -- GameManager.swift
+class GameManager {
+    
+    var scene: GameScene!
+    //1
+    var nextTime: Double?
+    var timeExtension: Double = 1
+    //1
+    var playerDirection: Int = 4
+    
+    init(scene: GameScene) {
+        self.scene = scene
+    }
+    
+    //1
+    func initGame() {
+        //starting player position
+        scene.playerPositions.append((10, 10))
+        scene.playerPositions.append((10, 11))
+        scene.playerPositions.append((10, 12))
+        renderChange()
+    }
+    
+    //2
+    func update(time: Double) {
+        if nextTime == nil {
+            nextTime = time + timeExtension
+        } else {
+            if time >= nextTime! {
+                nextTime = time + timeExtension
+                //2
+                updatePlayerPosition()
+            }
+        }
+    }
+    
+    //3
+    private func updatePlayerPosition() {
+        //4
+        var xChange = -1
+        var yChange = 0
+        //5
+        switch playerDirection {
+        case 1:
+            //left
+            xChange = -1
+            yChange = 0
+            break
+        case 2:
+            //up
+            xChange = 0
+            yChange = -1
+            break
+        case 3:
+            //right
+            xChange = 1
+            yChange = 0
+            break
+        case 4:
+            //down
+            xChange = 0
+            yChange = 1
+            break
+        default:
+            break
+        }
+        //6
+        if scene.playerPositions.count > 0 {
+            var start = scene.playerPositions.count - 1
+            while start > 0 {
+                scene.playerPositions[start] = scene.playerPositions[start - 1]
+                start -= 1
+            }
+            scene.playerPositions[0] = (scene.playerPositions[0].0 + yChange, scene.playerPositions[0].1 + xChange)
+        }
+        //7
+        renderChange()
+    }
+    
+    //2
+    func renderChange() {
+        for (node, x, y) in scene.gameArray {
+            if contains(a: scene.playerPositions, v: (x,y)) {
+                node.fillColor = SKColor.cyan
+            } else {
+                node.fillColor = SKColor.clear
+            }
+        }
+    }
+    
+    //3
+    func contains(a:[(Int, Int)], v:(Int,Int)) -> Bool {
+        let (c1, c2) = v
+        for (v1, v2) in a { if v1 == c1 && v2 == c2 { return true } }
+        return false
+    }
+}
